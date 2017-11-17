b9d5bc9129c606d5572d37f5946a29faaceb2944
Robert Kuczyński <robertkuczynski@gmail.com>
2017-11-17 10:59:28 +0100

    pliki z zadaniem 11-5

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+function Button(text) {
+	this.text = "Cześć, jak się masz?" || 'Hello';
+}
+
+Button.prototype = {
+create: function() {
+	var self = this;
+	this.$element = $('<button>');
+	this.$element.text(this.text);
+	this.$element.click(function() {
+		alert(self.text);
+	});
+  this.$element.appendTo($('body'));
+}
+}
+
+var btn1 = new Button();
+
+btn1.create();
